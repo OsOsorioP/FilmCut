102cfc16882d2d904ed080fd52a96833a5798340
workspace014-admin <oscar.osorio1884@gmail.com>
2024-12-12 11:38:47 -0500

    Change form

--- frontend/src/components/sessionForm/Form.tsx
+++ frontend/src/components/sessionForm/Form.tsx
@@ -21,22 +21,27 @@ interface LoginFormProps {
 
 export default function Form({ onClose }: LoginFormProps) {
 
-    const [value, setValue] = useState("")
+    const [value, setValue] = useState("login")
 
+    const handleTabChange = (newValue:string) => {
+        setValue(newValue);
+    };
+
+    console.log(value)
     return (
         <>
             <div className="flex flex-col md:flex-row justify-between gap-[20px]">
 
-                <Tabs defaultValue="login" className="flex flex-row md:flex-col justify-between text-center w-[500px] text-[#F6F6F6]">
+                <Tabs defaultValue="login" value={value} onValueChange={handleTabChange} className="flex flex-row md:flex-col justify-between text-center w-[500px] text-[#F6F6F6]">
                     <div className="flex gap-[7px] cursor-pointer pb-8 w-fit" onClick={onClose}>
                         <CircleArrowLeft />
                         <p>Back</p>
                     </div>
                     <div className="flex justify-center align-top ">
-                    <TabsList className="flex flex-row justify-center bg-[#262626] w-fit h-[46px] text-[#F6F6F6] ">
-                        <TabsTrigger value="register" onClick={() => setValue("register")} className="data-[state=active]:bg-[#F0B90B] data-[state=active]:text-[#F6F6F6] w-[116px] h-[46px]">Sign up</TabsTrigger>
-                        <TabsTrigger value="login" onClick={() => setValue("login")} className="data-[state=active]:bg-[#F0B90B] data-[state=active]:text-[#F6F6F6] w-[116px] h-[46px]" >Log In</TabsTrigger>
-                    </TabsList>
+                        <TabsList className="flex flex-row justify-center bg-[#262626] w-fit h-[46px] text-[#F6F6F6] ">
+                            <TabsTrigger value="register" onClick={() => (setValue("register"))} className="data-[state=active]:bg-[#F0B90B] data-[state=active]:text-[#F6F6F6] w-[116px] h-[46px]">Sign up</TabsTrigger>
+                            <TabsTrigger value="login" onClick={() => (setValue("login"))} className="data-[state=active]:bg-[#F0B90B] data-[state=active]:text-[#F6F6F6] w-[116px] h-[46px]" >Log In</TabsTrigger>
+                        </TabsList>
                     </div>
                     <TabsContent value="register">
                         <FormRegister />
@@ -47,19 +52,22 @@ export default function Form({ onClose }: LoginFormProps) {
                 </Tabs>
 
                 <div className="h-full flex flex-col justify-center align-middle items-center px-[20px] bg-[#1C1C1C] text-center text-[#FFFFFF] rounded-r-lg">
-                    {value === "register" ?
-                        (<>
-                            <h1 className="font-[700] text-[35px] leading-[39px] pt-[20px]" >Welcome to Quickbet Movies!</h1>
-                            <p className="font-[400] text-[20px] leading-[24px] pt-3">🎬 Ready to unlock a universe of cinematic delights? Sign up now and start your journey with us!</p>
-                            <Image src={characterRegister} alt={"character"} width={350} height={350} />
-                        </>)
-                        :
-                        (<>
-                            <h1 className="font-[700] text-[35px] leading-[39px] pt-[20px]" >Welcome Back to Quickbet Movie</h1>
-                            <p className="font-[400] text-[20px] leading-[24px] pt-3">🍿 Ready to dive into the world of unlimited entertainment? Enter your credentials and let the cinematic adventure begin!</p>
-                            <Image src={characterLogin} alt={"character"} width={350} height={350} />
-                        </>)}
+                    <h1 className="font-[700] text-[35px] leading-[39px] pt-[20px]">
+                        {value === "register" ? "Welcome to Quickbet Movies!" : "Welcome Back to Quickbet Movie"}
+                    </h1>
+                    <p className="font-[400] text-[20px] leading-[24px] pt-3">
+                        {value === "register"
+                            ? "🎬 Ready to unlock a universe of cinematic delights? Sign up now and start your journey with us!"
+                            : "🍿 Ready to dive into the world of unlimited entertainment? Enter your credentials and let the cinematic adventure begin!"}
+                    </p>
+                    <Image
+                        src={value === "register" ? characterRegister : characterLogin}
+                        alt="character"
+                        width={350}
+                        height={350}
+                    />
                 </div>
+
             </div>
         </>
     );
