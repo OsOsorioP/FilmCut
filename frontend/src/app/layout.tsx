7c92039dda687457ecd0cb5892d712968863fdf1
workspace014-admin <oscar.osorio1884@gmail.com>
2024-12-22 14:50:55 -0500

    Add loading lazy

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,8 @@ import SessionAuthProvider from "@/context/SessionAuthProvider";
 import Header from "@/components/ui/Header";
 import CustomModal from "@/components/forms/CustomModal";
 import { ModalProvider } from "@/context/ModalContext";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
+import { LoaderPinwheel } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "FilmCut",
@@ -22,16 +23,15 @@ export default function RootLayout({
         className={` bg-[#757575] antialiased overflow-x-hidden no-scrollbar`}
       >
 
-        <SessionAuthProvider>
-
-          <ModalProvider>
-            <Header />
-            {children}
-            <CustomModal />
-          </ModalProvider>
-
-        </SessionAuthProvider>
-
+        <Suspense fallback={<div className="fixed top-0 left-0"><LoaderPinwheel/></div>}>
+          <SessionAuthProvider>
+            <ModalProvider>
+              <Header />
+              {children}
+              <CustomModal />
+            </ModalProvider>
+          </SessionAuthProvider>
+        </Suspense>
       </body>
     </html>
   );
