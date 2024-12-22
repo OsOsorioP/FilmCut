f214560dd752a1ee85a7876455d452f033a69a5d
workspace014-admin <oscar.osorio1884@gmail.com>
2024-12-22 13:59:48 -0500

    Change component ScrollToUp

--- frontend/src/components/ui/ScrollToUp.tsx
+++ frontend/src/components/ui/ScrollToUp.tsx
@@ -1,44 +1,43 @@
-"use client";
+"use client"
 import { ChevronsUp } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function ScrollToUp() {
-  const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const toggleVisibility = () => {
-      if (window.scrollY > 300) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", toggleVisibility);
+
+        return () => {
+            window.removeEventListener("scroll", toggleVisibility);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     };
 
-    window.addEventListener("scroll", toggleVisibility);
-
-    return () => {
-      window.removeEventListener("scroll", toggleVisibility);
-    };
-  }, []);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
-  return (
-    <div className="fixed bottom-4 right-4 z-50">
-      {isVisible && (
-        <button
-          onClick={scrollToTop}
-          className="bg-gray-800 text-white p-3 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          aria-label="Scroll to top"
-        >
-          <ChevronsUp className="h-6 w-6" />
-        </button>
-      )}
-    </div>
-  );
+    return (
+        <div className="fixed bottom-4 right-4">
+            {isVisible && (
+                <button
+                    onClick={scrollToTop}
+                    className="bg-[#262626] text-white p-3 rounded-full shadow-lg"
+                >
+                    <ChevronsUp className="h-6 w-6" />
+                </button>
+            )}
+        </div>
+    );
 }
